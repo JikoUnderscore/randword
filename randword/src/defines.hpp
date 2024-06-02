07c54eeded498277f7db7be09ac8bede9e1308d5
JikoUnderscore <j1ko@abv.bg>
2024-06-02 16:11:32 +0300

    added gsl::owner

--- randword/src/defines.hpp
+++ randword/src/defines.hpp
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <utility>
 
-#define NODIS [[nodiscard]] 
+#define NODIS [[nodiscard]]
 #define cast(T, expr) static_cast<T>((expr))
 
 
@@ -85,6 +85,21 @@ using isize = ptrdiff_t;
 // }
 
 
+//
+// owner
+//
+// `gsl::owner<T>` is designed as a safety mechanism for code that must deal directly with raw pointers that own memory.
+// Ideally such code should be restricted to the implementation of low-level abstractions. `gsl::owner` can also be used
+// as a stepping point in converting legacy code to use more modern RAII constructs, such as smart pointers.
+//
+// T must be a pointer type
+// - disallow construction from any type other than pointer type
+//
+namespace gsl {
+template <class T, class = std::enable_if_t<std::is_pointer<T>::value>>
+using owner = T;
+
+}
 template <typename F>
 struct gbprivDefer {
     F f;
